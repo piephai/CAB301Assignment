1848cf6657999fb2f7eb239948281f1a26d0e3d1
piephai <tonphai2522@gmail.com>
2020-05-12 18:40:05 +1000

    Test

--- CAB301Assignment/Program.cs
+++ CAB301Assignment/Program.cs
@@ -6,7 +6,7 @@ namespace CAB301Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Yes");
         }
     }
 }
